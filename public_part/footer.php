830765b6748b4d6e52f5f9d6f01ab38b0c785390
supriynt <riyan060898@gmail.com>
2019-05-17 13:36:18 +0700

    suprinew

--- public_part/footer.php
+++ public_part/footer.php
@@ -1,8 +1,10 @@
 
 <div class="footer">
 	<div class="container-fluid">
-		<div class="jumbotron">
+		<div class="p-3 mb-2 bg-primary text-dark">
+		<div class="jumbotron col-md-12">
 			<h1>@Copyright</h1>
+		</div>
 		</div>
 	</div>
 </div>
