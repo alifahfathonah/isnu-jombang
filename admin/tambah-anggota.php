febeb710639b6b8fa5ce7ec3f10e5c7ebae72bad
adiidx <anonyzombies@gmail.com>
2019-06-17 10:23:23 +0700

    adiidx

--- admin/tambah-anggota.php
+++ admin/tambah-anggota.php
@@ -3,193 +3,183 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Tambah Angota</title>
-	<!-- link bootstrap -->
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- link style.css -->
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-</head>
-<body>
-	<!-- navbar -->
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
-  	<div class="card" style="width: 3rem;">
-  		<img src="../assets/gambar/header.png" class="card-img-top" alt="...">
-	</div>
-  	<h4 class="text-white">Ikatan Sarjana Nahdlatul Ulama</h4>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit" style="margin-right: 20px">Search</button>
-      <hr>
-      <button type="button" class="btn btn-secondary">Logout</button>
-    </form>
-  </div>
-  </div>
-</nav>
-<!-- akhir navbar -->
-<!-- sidebar -->
-<div class="container-fluid" style="margin-top: 80px">
-	<div class="row">
-
-		<div class="col-md-3">
-			<div class="card">
-			  <div class="card-body">
-			    <h5 class="card-title text-center"><i class="fa fa-list-ul"></i > MAIN MENU</h5>
-			    <hr>
-					<ul class="list-group">
-					  <li class="list btn btn-danger text-center" type="button" ><i class="fa fa-home"></i>
-					  DASHBOARD
-					  </li>
-					  <br>
-
-					  <!-- Example single danger button -->
-						<div class="btn-group">
-						  <button type="button" class="btn btn-danger fa fa-users dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						    DATA PENDAFTAR
-						  </button>
-						  <div class="dropdown-menu">
-						    <a class="dropdown-item" href="#">Data Pendaftar</a>
-						    <a class="dropdown-item" href="#">Edit Data Pendaftar</a>
-						    <a class="dropdown-item" href="#">Deleter Data Pendaftar</a>
-						  </div>
-						</div>
-					<ul class="list-group">
-					  <li class="list btn-outline-secondary text-center" type="button" ><i class="fa fa-home"></i>
-					  DASHBOARD
-					  </li>
-					  <br>
-					  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-					    <a class="dropdown-item" href="#">Data Anggota</a>
-					    <a class="dropdown-item" href="#">Edit Data Anggota</a>
-					    <a class="dropdown-item" href="#">Delete Data Anggota</a>
-					  </div>
-					  <br>
-					  <li class="list btn btn-danger text-center" type="button"><i class="fa fa-users"></i> USER
-					  </li>
-					</ul>
-			  </div>
-			</div>
-		</div>
-<!-- akhir sidebar -->
-<!-- form -->
-		<div class="col-md-9">
-			<div class="card">
-			  <div class="card-body">
-			    	<h5 class="card-title text-center"><i class="fa fa-list-ul"></i> FORM DATA SARJANA NU JOMBANG</h5>
-            	<hr>
-
-              <form method="post" action="simpan-anggota.php">
-                
-                <div class="form-group">
-                  <label for="exampleInputEmail1">NAMA LENGKAP</label>
-                  <input type="text" name="fnama" class="form-control" placeholder="Masukkan Nama Lengkap Anda">
-                </div>
-                
-                <div class="form-group">
-                  <label for="exampleInputPassword1">NO. KTP</label>
-                  <input type="text" name="fno_ktp" class="form-control" placeholder="Masukkan No. KTP Anda">
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleFormControlSelect1">Jenis Kelamin</label>
-                  <input type="text" name="fjenis_kelamin" class="form-control" placeholder="Jenis Kelamin">
-                  <!--
-                  <select class="form-control" id="exampleFormControlSelect1">
-                    <option>Laki-Laki</option>
-                    <option>Perempuan</option>
-                  </select>
-                  -->
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Tempat Lahir</label>
-                  <input type="text" name="ftempat_lahir" class="form-control" placeholder="Tempat Lahir">
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Tanggal Lahir</label>
-                  <input type="text" name="ftgl_lahir" class="form-control" placeholder="Tanggal Lahir">
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Dusun/Desa</label>
-                  <input type="text" name="fdusun_desa" class="form-control" placeholder="Dusun/Desa">
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleFormControlSelect1">Kecamatan</label>
-                  <input type="text" name="fkecamatan" class="form-control" placeholder="Kecamatan">
-                  <!--
-                  <select class="form-control" id="exampleFormControlSelect1">
-                    <option>Lain-lain</option>
-                    <option>Jombang</option>
-                    <option>Mojokerto</option>
-                  </select>
-                  -->
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Kota</label>
-                  <input type="text" name="fkota" class="form-control" placeholder="Kota"><!-- disabled=""> -->
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Nomor Telepon</label>
-                  <input type="text" name="fno_telepon" class="form-control" placeholder="Masukkan No. Telepon">
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleFormControlSelect1">Jenjang</label>
-                  <input type="text" name="fjenjang" class="form-control" placeholder="Jenjang">
-                  <!--
-                  <select class="form-control" id="exampleFormControlSelect1">
-                    <option>Lain-lain</option>
-                    <option>S1</option>
-                  </select>
-                  -->
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Jurusan</label>
-                  <input type="text" name="fjurusan" class="form-control" placeholder="Masukkan Jurusan">
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleInputPassword1">PT / Universitas</label>
-                  <input type="text" name="fpt_univ" class="form-control" placeholder="Asal PT/Universitas">
-                </div>
-
-                <!--
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Recaptcha</label>
-                  <input type="text" class="form-control" placeholder="">
-                </div>
-
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Recaptcha</label>
-                  <input type="text" class="form-control" placeholder="">
-                </div>
-
-                <form>
-                  <div class="form-group">
-                    <label for="exampleFormControlFile1">File - EDITABLE</label>
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                    (max. 1MB)
-                  </div>
-                </form>
-                -->
-
-                <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> DAFTAR</button>
-                <button type="reset" class="btn btn-warning"><i class="fas fa-undo"></i> RESET</button>
-
-              </form>
+	<head>
+		<title>Tambah Angota</title>
+		<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="../assets/css/style.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	</head>
+	<body>
+		<!-- navbar -->
+		<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+			<div class="container">
+				<div class="card" style="width: 3rem;">
+					<img src="../assets/gambar/header.png" class="card-img-top" alt="...">
 				</div>
+				<h4 class="text-white">Ikatan Sarjana Nahdlatul Ulama</h4>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-secondary my-2 my-sm-0" type="submit" style="margin-right: 20px">Search</button>
+					<hr>
+					<button type="button" class="btn btn-secondary">Logout</button>
+				</form>
+			</div>
+		</nav>
+		<!-- end navbar -->
+		<!-- sidebar -->
+		<div class="container-fluid" style="margin-top: 80px">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title text-center"><i class="fa fa-list-ul"></i>MAIN MENU</h5>
+							<hr>
+							<ul class="list-group">
+								<li class="list btn btn-danger text-center" type="button" ><i class="fa fa-home"></i>
+									DASHBOARD
+								</li><br>
+								<div class="btn-group">
+									<button type="button" class="btn btn-danger fa fa-users dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">DATA PENDAFTAR</button>
+									<div class="dropdown-menu">
+										<a class="dropdown-item" href="#">Data Pendaftar</a>
+										<a class="dropdown-item" href="#">Edit Data Pendaftar</a>
+										<a class="dropdown-item" href="#">Deleter Data Pendaftar</a>
+									</div>
+								</div>
+							</ul>
+							<ul class="list-group">
+								<li class="list btn-outline-secondary text-center" type="button" ><i class="fa fa-home"></i>
+									DASHBOARD
+								</li><br>
+								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+									<a class="dropdown-item" href="#">Data Anggota</a>
+									<a class="dropdown-item" href="#">Edit Data Anggota</a>
+									<a class="dropdown-item" href="#">Delete Data Anggota</a>
+								</div><br>
+								<li class="list btn btn-danger text-center" type="button"><i class="fa fa-users"></i>USER
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- end sidebar -->
+				<!-- form -->
+				<div class="col-md-9">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title text-center"><i class="fa fa-list-ul"></i> FORM DATA SARJANA NU JOMBANG</h5>
+							<hr>
+
+							<form method="post" action="simpan-anggota.php">
+
+								<div class="form-group">
+									<label for="exampleInputEmail1">NAMA LENGKAP</label>
+									<input type="text" name="fnama" class="form-control" placeholder="Nama">
+								</div>
+								                
+								<div class="form-group">
+									<label for="exampleInputPassword1">NO. KTP</label>
+									<input type="text" name="fno_ktp" class="form-control" placeholder="No. KTP">
+								</div>
+
+								<div class="form-group">
+									<label for="exampleFormControlSelect1">Jenis Kelamin</label>
+									<!--
+									<input type="text" name="fjenis_kelamin" class="form-control" placeholder="Jenis Kelamin">
+									-->
+
+									<select name="fjenis_kelamin" class="form-control" id="exampleFormControlSelect1">
+										<option>Laki-Laki</option>
+										<option>Perempuan</option>
+									</select>
+								</div>
+
+								<div class="form-group">
+									<label for="exampleInputPassword1">Tempat Lahir</label>
+									<input type="text" name="ftempat_lahir" class="form-control" placeholder="Tempat Lahir">
+								</div>
+
+								<div class="form-group">
+									<label for="exampleInputPassword1">Tanggal Lahir</label>
+									<input type="text" name="ftgl_lahir" class="form-control" placeholder="Tanggal Lahir">
+								</div>
+
+								<div class="form-group">
+									<label for="exampleInputPassword1">Dusun/Desa</label>
+									<input type="text" name="fdusun_desa" class="form-control" placeholder="Dusun/Desa">
+								</div>
+
+								<div class="form-group">
+									<label for="exampleFormControlSelect1">Kecamatan</label>
+									<input type="text" name="fkecamatan" class="form-control" placeholder="Kecamatan">
+									<!--
+									<select class="form-control" id="exampleFormControlSelect1">
+									<option>Lain-lain</option>
+									<option>Jombang</option>
+									<option>Mojokerto</option>
+									</select>
+									-->
+								</div>
+
+								<div class="form-group">
+									<label for="exampleInputPassword1"></label>
+									<input type="text" name="fkota" class="form-control" placeholder="Kota"><!-- disabled=""> -->
+								</div>
+
+								<div class="form-group">
+									<label for="exampleInputPassword1">Nomor Telepon</label>
+									<input type="text" name="fno_telepon" class="form-control" placeholder="Masukkan No. Telepon">
+								</div>
+
+								<div class="form-group">
+									<label for="exampleFormControlSelect1">Jenjang</label>
+									<input type="text" name="fjenjang" class="form-control" placeholder="Jenjang">
+									<!--
+									<select class="form-control" id="exampleFormControlSelect1">
+									<option>Lain-lain</option>
+									<option>S1</option>
+									</select>
+									-->
+								</div>
+
+								<div class="form-group">
+									<label for="exampleInputPassword1">Jurusan</label>
+									<input type="text" name="fjurusan" class="form-control" placeholder="Masukkan Jurusan">
+								</div>
+
+								<div class="form-group">
+									<label for="exampleInputPassword1">PT / Universitas</label>
+									<input type="text" name="fpt_univ" class="form-control" placeholder="Asal PT/Universitas">
+								</div>
+
+								<!--
+								<div class="form-group">
+									<label for="exampleInputPassword1">Recaptcha</label>
+									<input type="text" class="form-control" placeholder="">
+								</div>
+
+								<div class="form-group">
+									<label for="exampleInputPassword1">Recaptcha</label>
+									<input type="text" class="form-control" placeholder="">
+								</div>
+
+								<div class="form-group">
+									<label for="exampleFormControlFile1">File - EDITABLE</label>
+									<input type="file" class="form-control-file" id="exampleFormControlFile1">
+									(max. 1MB)
+								</div>
+								-->
+
+								<button type="submit" class="btn btn-success"><i class="fa fa-save"></i>TAMBAHKAN</button>
+								<button type="reset" class="btn btn-warning"><i class="fas fa-undo"></i>RESET</button>
+
+							</form>
+						</div>
+					</div>
+				</div>
+				<!-- akhir form -->
 			</div>
 		</div>
-
-<!-- akhir form -->
-</body>
+	</body>
 </html>
