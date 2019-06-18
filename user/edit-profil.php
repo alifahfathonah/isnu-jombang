2ad135d1bb4c93573153487f666de418884c53da
adiidx <anonyzombies@gmail.com>
2019-06-18 07:47:35 +0700

    adiidx

--- user/edit-profil.php
+++ user/edit-profil.php
@@ -46,96 +46,113 @@
 					<h5 class="card-title text-center"><i class="fa fa-list-ul"></i>FORM DATA SARJANA NU JOMBANG</h5>
 					<hr>
 
-					<form>
-						<div class="form-group">
-							<label for="exampleInputEmail1">NAMA LENGKAP</label>
-							<input type="text" class="form-control" placeholder="Masukkan Nama Lengkap Anda">
-						</div>
+					<form method="post" action="update-profil.php">
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">NO. KTP</label>
-							<input type="text" class="form-control" placeholder="Masukkan No. KTP Anda">
-						</div>
+							<div class="form-group">
+								<label>Nama</label>
+								<input type="text" name="fnama" class="form-control" placeholder="Nama">
+							</div>
+							                
+							<div class="form-group">
+								<label>No. KTP</label>
+								<input type="text" name="fno_ktp" maxlength="16" onkeypress="return hanyaAngka(event)" class="form-control" placeholder="No. Telepon">
+								<script>
+									function hanyaAngka(evt) {
+									  	var charCode = (evt.which) ? evt.which : event.keyCode
+									   	if (charCode > 31 && (charCode < 48 || charCode > 57))
+							 
+									    return false;
+									  	return true;
+									}
+								</script>
+							</div>
 
-						<div class="form-group">
-							<label for="exampleFormControlSelect1">Jenis Kelamin</label>
-							<select class="form-control" id="exampleFormControlSelect1">
-								<option>Laki-Laki</option>
-								<option>Perempuan</option>
-							</select>
-						</div>
+							<div class="form-group">
+								<label>Jenis Kelamin</label>
+								<select name="fjenis_kelamin" class="form-control">
+									<option>Laki-laki</option>
+									<option>Perempuan</option>
+								</select>
+							</div>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">Tempat Lahir</label>
-							<input type="text" class="form-control" placeholder="Tempat Lahir">
-						</div>
+							<div class="form-group">
+								<label>Tempat Lahir</label>
+								<input type="text" name="ftempat_lahir" class="form-control" placeholder="Tempat Lahir">
+							</div>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">Tanggal Lahir</label>
-							<input type="text" class="form-control" placeholder="Tanggal Lahir">
-						</div>
+							<div class="form-group">
+								<label>Tanggal Lahir</label>
+								<input type="text" name="ftgl_lahir" class="form-control" placeholder="YYYY-MM-DD">
+							</div>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">Alamat</label>
-							<input type="text" class="form-control" placeholder="Dusun/Desa">
-						</div>
+							<div class="form-group">
+								<label>Alamat</label>
+								<input type="text" name="fdusun_desa" class="form-control" placeholder="Dusun/Desa">
+								<br>
+								<select name="fkecamatan" class="form-control">
+									<?php
+									  	$qTampilKecamatan = "SELECT * FROM kecamatan";
+									  	$tampilKecamatan = mysqli_query($connect, $qTampilKecamatan);
+										while($row = mysqli_fetch_assoc($tampilKecamatan) ){
+									?>
 
-						<div class="form-group">
-							<label for="exampleFormControlSelect1"></label>
-							<select class="form-control" id="exampleFormControlSelect1">
-								<option>Lain-lain</option>
-								<option>Jombang</option>
-								<option>Mojokerto</option>
-							</select>
-						</div>
+   									<option value="<?php echo $row['kecamatan']; ?>"><?php echo $row['kecamatan']; ?></option>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1"></label>
-							<input type="text" class="form-control" placeholder="" disabled="">
-						</div>
+									<?php 
+										} 
+									?>
+								</select>
+								<br>
+								<input type="text" name="fkota" class="form-control" value="Kab. Jombang" readonly>
+							</div>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">Nomor Telepon</label>
-							<input type="text" class="form-control" placeholder="Masukkan No. Telepon">
-						</div>
+							<div class="form-group">
+								<label>Nomor Telepon</label>
+								<input type="text" name="fno_telepon" maxlength="13" onkeypress="return hanyaAngka(event)" class="form-control" placeholder="No. Telepon">
+								<script>
+									function hanyaAngka(evt) {
+									  	var charCode = (evt.which) ? evt.which : event.keyCode
+									   	if (charCode > 31 && (charCode < 48 || charCode > 57))
+								 
+									    return false;
+									  	return true;
+									}
+								</script>
+							</div>
 
-						<div class="form-group">
-							<label for="exampleFormControlSelect1">Jenjang</label>
-							<select class="form-control" id="exampleFormControlSelect1">
-								<option>Lain-lain</option>
-								<option>S1</option>
-							</select>
-						</div>
+							<div class="form-group">
+								<label>Jenjang</label>
+								<select name="fjenjang" class="form-control">
+									<option>S1</option>
+									<option>S2</option>
+									<option>S3</option>
+								</select>
+							</div>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">Jurusan</label>
-							<input type="text" class="form-control" placeholder="Masukkan Jurusan">
-						</div>
+							<div class="form-group">
+								<label>Jurusan</label>
+								<input type="text" name="fjurusan" class="form-control" placeholder="Jurusan">
+							</div>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">PT / Universitas</label>
-							<input type="text" class="form-control" placeholder="Asal PT/Universitas">
-						</div>
+							<div class="form-group">
+								<label>PT / Universitas</label>
+								<input type="text" name="fpt_univ" class="form-control" placeholder="Asal PT/Universitas">
+							</div>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">Recaptcha</label>
-							<input type="text" class="form-control" placeholder="">
-						</div>
+							<div class="g-recaptcha" data-sitekey="6LdLMKkUAAAAAHap-4beGm4OPyKP3f9nNqjbidV1"></div><br>
 
-						<div class="form-group">
-							<label for="exampleInputPassword1">Recaptcha</label>
-							<input type="text" class="form-control" placeholder="">
-						</div>
-						
-						<div class="form-group">
-							<label for="exampleFormControlFile1">File - EDITABLE</label>
-							<input type="file" class="form-control-file" id="exampleFormControlFile1">
-							(max. 1MB)
-						</div>
+							<!--
+							<div class="form-group">
+								<label>File - EDITABLE</label>
+								<input type="file" class="form-control-file">
+								(max. 1MB)
+							</div>
+							-->
+							
+							<button type="submit" class="btn btn-success" id="button1"><i class="fa fa-save"></i>TAMBAHKAN</button>
+							<button type="reset" class="btn btn-warning"><i class="fas fa-undo"></i>RESET</button>
 
-						<button type="submit" class="btn btn-success"><i class="fa fa-save"></i>DAFTAR</button>
-						<button type="reset" class="btn btn-warning"><i class="fas fa-undo"></i>RESET</button>
-					</form>
+						</form>
 				</div>
 			</div>
 		</div>
