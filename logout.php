f4f57e4214943962efd42ab50f07eda0a3ee66b1
adiidx <anonyzombies@gmail.com>
2019-06-19 10:09:24 +0700

    adiidx

--- logout.php
+++ logout.php
@@ -1,10 +1,5 @@
-<?php 
-	// mengaktifkan session php
+<?php
 	session_start();
-	 
-	// menghapus semua session
 	session_destroy();
-	 
-	// mengalihkan halaman ke halaman login
 	header("location:../login/index.php");
 ?>
