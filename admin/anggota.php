9d513cdb9a39e0e32fe8bca9fe1ecec0990a9db1
adiidx <anonyzombies@gmail.com>
2019-06-27 10:15:28 +0700

    adiidx

--- admin/anggota.php
+++ admin/anggota.php
@@ -15,35 +15,35 @@
 	    	<blockquote class="blockquote mb-0">
 				<table class="table table-striped table-bordered" id="table_id">
 					<thead>
-					<tr>
-						<th><strong>No</strong></th>
-						<th><strong>Nama</strong></th>
-						<th><strong>Kecamatan</strong></th>
-						<th><strong>Jurusan</strong></th>
-						<th><center><strong>Aksi</strong></center></th>
-					</tr>
+						<tr>
+							<th><strong>No</strong></th>
+							<th><strong>Nama</strong></th>
+							<th><strong>Kecamatan</strong></th>
+							<th><strong>Jurusan</strong></th>
+							<th><center><strong>Aksi</strong></center></th>
+						</tr>
 					</thead>
 					<tbody>
-					<?php
-						$no = 1;
-					  	$qTampil = mysqli_query($connect, "SELECT * FROM anggota");
-					  	foreach($qTampil as $row){
-					?>
+						<?php
+							$no = 1;
+						  	$qTampil = mysqli_query($connect, "SELECT * FROM anggota");
+						  	foreach($qTampil as $row){
+						?>
 
-					<tr>
-						<td><?php echo $no++; ?></td>
-						<td><?php echo $row['nama']; ?></td>
-						<td><?php echo $row['kecamatan']; ?></td>
-						<td><?php echo $row['jurusan']; ?></td>
-						<td>		
-							<a class="btn btn-success btn-sm" href='edit-anggota.php?id=<?php echo $row['id']; ?>'><i class="fa fa-edit"></i> Edit</a> <a class="btn btn-danger btn-sm" href='hapus-anggota.php?id=<?php echo $row['id']; ?>'><i class="fa fa-trash"></i> Hapus</a>
-						</td>
-					</tr>
+						<tr>
+							<td><?php echo $no++; ?></td>
+							<td><?php echo $row['nama']; ?></td>
+							<td><?php echo $row['kecamatan']; ?></td>
+							<td><?php echo $row['jurusan']; ?></td>
+							<td>		
+								<a class="btn btn-success btn-sm" href='edit-anggota.php?id=<?php echo $row['id']; ?>'><i class="fa fa-edit"></i> Edit</a> <a class="btn btn-danger btn-sm" href='hapus-anggota.php?id=<?php echo $row['id']; ?>'><i class="fa fa-trash"></i> Hapus</a>
+							</td>
+						</tr>
 
-					<?php
-						}
-					?>
-				</tbody>
+						<?php
+							}
+						?>
+					</tbody>
 				</table><br>
 	    	</blockquote>
 	  	</div>
