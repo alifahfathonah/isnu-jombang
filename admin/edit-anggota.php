dc41292afebbace24fb1337e635b8c1ba9d64e02
adiidx <anonyzombies@gmail.com>
2019-06-28 10:33:50 +0700

    adiidx

--- admin/edit-anggota.php
+++ admin/edit-anggota.php
@@ -71,15 +71,21 @@
 							<select name="fkecamatan" class="form-control" required>
 								<option value="">--Pilih Kecamatan--</option>
 								<?php
-								  	$qTampilKecamatan = "SELECT * FROM kecamatan";
-								  	$tampilKecamatan = mysqli_query($connect, $qTampilKecamatan);
+									$selectedKecamatan	= $row['kecamatan'];
+								  	$qTampilKecamatan 	= "SELECT * FROM kecamatan";
+								  	$tampilKecamatan 	= mysqli_query($connect, $qTampilKecamatan);
 									while($rows = mysqli_fetch_assoc($tampilKecamatan) ){
+										if($rows['kecamatan'] == $selectedKecamatan){
 								?>
-									
+								<option value="<?php echo $rows['kecamatan']; ?>" selected="selected"><?php echo $rows['kecamatan']; ?></option>
+								<?php
+								        }else{
+								?>
 								<option value="<?php echo $rows['kecamatan']; ?>"><?php echo $rows['kecamatan']; ?></option>
 
 								<?php 
 									} 
+								}
 								?>
 							</select>
 							<br>
